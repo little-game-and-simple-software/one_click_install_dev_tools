03e6fac9bef6005a9495eb612faa2708df11e515
2439905184 <suchenglong018@126.com>
2020-07-14 11:51:54 +0800

    添加了rapid环境变量配置工具的下载

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
 import os
-import requests
 import time
+import json
+from pip._vendor import requests
+#使用demjson解析json文件
+import demjson
 download_urls=[]
 current_dir=os.path.dirname(__file__)
 def visual_studio_code():
@@ -26,20 +29,45 @@ def torisorgit():
     with open(os.path.join(current_dir,"TortoiseGit-2.10.0.2-32bit.msi"),"wb")as f:
         f.write(res.content)
     pass
+#环境变量配置工具
+def env():
+    print("环境变量配置工具下载开始")
+    res=requests.get("https://www.rapidee.com/download/RapidEE_setup.exe")
+    with open(os.path.join(current_dir,"RapidEE_setup.exe"),"wb") as f:
+        f.write(res.content)
+    pass
+#下载核心(单线程)
+def download_core(url,tool_name):
+    res=requests.get(url)
+    with open(os.path.join(current_dir,tool_name)) as f:
+        f.write(res.content)
+    pass
 #帮助doc
 def help():
+    os.system("cls")
+    print("下载可能有点慢")
     print("欢迎来到帮助页面 首先你可以修改json文件来修改下载链接, 你也可以添加自己的新的开发工具的下载地址 \n 注意必须是直链 不然不行")
-    pass
+    print("输入0回到上一层")
+    print("作者微信:13023335265欢迎交流")
+    tmp=input()
+    if tmp=="0":
+        print("cls清屏")
+        os.system("cls")
+        main()
 #读取json配置文件
 def read_config():
-
+    file=open(os.path.join(current_dir,"config.json"))
+    s=file.read()
+    demjson.encode(s)
+    file.close()
     pass
 #主方法
 def main():
     print("请输入选项")
-    print("输入1一键下载安装vs_code,notepad++,git,torisorgit")
+    print("下载可能有点慢")
+    print("输入1一键下载安装vs_code编辑器, notepad++编辑器,git版本控制,torisorgit版本控制,环境变量配置工具")
     print("输入help来获得帮助")
-    print("你可以手动编写json文件来修改下载地址")
+    print("你可以手动编写json文件来修改下载地址(功能没写完h)")
     print("输入0退出")
     usr_in=input()
     if usr_in=="1":
@@ -48,6 +76,8 @@ def main():
         notepad_add()
         git()
         torisorgit()
+        env()
+        print("所有开发工具下载完成,快去看看吧")
     if usr_in=="0":
         print("退出！0")
         pass
@@ -55,5 +85,6 @@ def main():
         help()
         pass
     pass
-
+#---TODO添加读取json文件的功能
+#read_config()
 main()
